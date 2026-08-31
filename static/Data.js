8b4cb90d81fb89dd4e9d830f600a9f61fd9b89f6
SxtxnixTH <rachaphat44@gmail.com>
2026-08-31 23:23:26 +0700

    Update print statement from 'Hello' to 'Goodbye'

--- static/Data.js
+++ static/Data.js
@@ -468,17 +468,6 @@ function ensureDeployReady() {
                 );
             }
 
-            /* Deploy.js */
-            if (
-                !window.AutoGenDeploy ||
-                typeof window.AutoGenDeploy.generate !== "function"
-            ) {
-
-                await loadExternalScript(
-                    "Deploy.js"
-                );
-            }
-
             if (
                 !window.AutoGenDeploy ||
                 typeof window.AutoGenDeploy.generate !== "function"
@@ -487,6 +476,7 @@ function ensureDeployReady() {
                     "Deploy.js did not initialize AutoGenDeploy."
                 );
             }
+
         })();
 
     return deployReadyPromise;
